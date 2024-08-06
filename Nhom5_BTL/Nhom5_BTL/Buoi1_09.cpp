1867b0ace361fd973433a0daf07ef3e1c2c181bc
nguyenthanhtien3214 <nguyenthanhtien3214@gmail.com>
2024-08-06 23:25:21 +0700

    Buoi1_09 : Xét lại bài 3, tính các giá trị tổng, hiệu, tích, thương của 2 số a, b thông qua các con trỏ pa, pb. Xuất ra kết quả và địa chỉ các ô nhớ chứa tổng, hiệu, tích thương đó.

---
+++ Nhom5_BTL/Nhom5_BTL/Buoi1_09.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <stdio.h>
+
+int main() {
+
+    int a, b;
+
+    printf("Nhap vao gia tri cua a: ");
+    scanf_s("%d", &a);
+    printf("Nhap vao gia tri cua b: ");
+    scanf_s("%d", &b);
+
+
+    int* pa = &a;
+    int* pb = &b;
+
+    int sum, diff, product;
+    float quotient;
+
+    sum = *pa + *pb;
+    diff = *pa - *pb;
+    product = (*pa) * (*pb);
+    if (*pb != 0) {
+        quotient = (float)(*pa) / (*pb);
+    }
+    else {
+        quotient = 0.0;
+    }
+
+    printf("Tong: %d, dia chi: %p\n", sum, (void*)&sum);
+    printf("Hieu: %d, dia chi: %p\n", diff, (void*)&diff);
+    printf("Tich: %d, dia chi: %p\n", product, (void*)&product);
+    printf("Thuong: %f, dia chi: %p\n", quotient, (void*)&quotient);
+
+    return 0;
+}
